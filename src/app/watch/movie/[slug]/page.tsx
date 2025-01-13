d7310892a747fda674f6a70e5b9dc5970c02363d
nero31994 <arenaofvalorph937@gmail.com>
2025-01-13 10:49:53 +0800

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -5,5 +5,5 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://multiembed.mov/?video_id=${id}`} />;
+  return <EmbedPlayer url={`https://multiembed.mov/?video_${id}`} />;
 }
