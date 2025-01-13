489646d06b7a2c0333187810141698f8278e19bf
nero31994 <arenaofvalorph937@gmail.com>
2025-01-14 07:19:10 +0800

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -5,6 +5,5 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://vidsrc.xyz/embed/movie/${id}`} />;
+  return <EmbedPlayer url={`https://embed.su/embed/movie/${id}`} />;
 }
-<iframe src="https://vidsrc.me/" referrerpolicy="origin"></iframe>
